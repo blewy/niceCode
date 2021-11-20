7d5299ab3828ae62f2b59665623e512ae7941279
João Santos <jfasantos@gmail.com>
2021-11-20 10:59:55 +0000

    adds tests to code and to actions workflow

---
+++ test_fizzbuzz.py
@@ -0,0 +1,14 @@
+"""Perform tests of the fizz_buzz function."""
+from fizzbuzz import fizz_buzz
+import pytest
+
+inputs = [3, 5, 15, 4, 10, 115, 7]
+outputs = ["fizz", "buzz", "fizzbuzz", "4", "buzz", "buzz", "7"]
+
+
+@pytest.mark.parametrize("inp,out", zip(inputs, outputs))
+def test_fizzbuzz(inp, out):
+    """Takes inputs, gets the output of the fizz_buzz function.
+    Asserts whether equality holds.
+    """
+    assert fizz_buzz(inp) == out
